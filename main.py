6cc1fe8b11c918da490352259a1c5ffbbb0b04fd
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2022-02-01 00:27:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ def start(update, context):
     context.bot.send_message(chat_id=user_id, text='стартуем')
 
     context.bot.send_message(chat_id=user_id, text='Работаем')
-    while user_id!= 997836793:
+    while 467657!= 997836793:
         context.bot.send_message(chat_id=user_id, text='Ебашим')
         sleep(1800)
+ def star(update, context):
+    user_id = update.message.chat_id
+    context.bot.send_message(chat_id=user_id, text='стартуем')
+
+    context.bot.send_message(chat_id=user_id, text='Работаем')
+      
 TOKEN ='5088514755:AAHh705K_7aQ65Ic1yPkGm23osNmlrmi_V4'
 upd = Updater(token=TOKEN, workers=4)
 dis = upd.dispatcher
 dis.add_handler(CommandHandler(command='start', callback=start))
+dis.add_handler(CommandHandler(command='star', callback=star))
 upd.start_polling(drop_pending_updates=True)
